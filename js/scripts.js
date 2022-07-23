958d192cf40d24189837268639941932e928920c
Fredo0888 <fredexact@gmail.com>
2022-07-23 11:00:30 +0300

    commit_and_push_6

--- js/scripts.js
+++ js/scripts.js
@@ -5,7 +5,7 @@ $(document).ready(function(){
         margin:0,
         nav:true,
         autoplay:true,
-        autoplayTimeout:3000,
+        autoplayTimeout: 5000,
         navText: [ '', ' ' ],
 
         responsive:{
